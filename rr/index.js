06b40cb2c9ae5128d12ad5fb7ba5bc6ee31c2930
kashbird <artisthg122@gmail.com>
2022-04-12 13:46:16 +0900

    first

---
+++ rr/index.js
@@ -0,0 +1,18 @@
+// let gnbli = document.querySelector('.gnb li');
+// let lnb = document.querySelector('.lnb');
+
+// // gnbli.addEventListener('mouseenter', () => {
+// //   if(lnb.style.transform === 'scaleY(0)') {
+// //     lnb.style.transform = 'scaleY(1)';
+// //   } else {
+// //     lnb.style.transform = 'scaleY(0)';
+// //   }
+// // })
+
+// gnbli.addEventListener('mouseenter', () => {
+//   lnb.style.transform = 'scaleY(1)';
+// })
+
+// gnbli.addEventListener('mouseleave', () => {
+//   lnb.style.transform = 'scaleY(0)';
+// })
